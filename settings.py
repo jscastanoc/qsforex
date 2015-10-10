c610d8567514c344ca01e2417dd475db33c4a062
Sebastian Castano <jscastanoc@gmail.com>
2015-10-10 18:24:09 +0200

    add (un-tested) implementation

--- settings.py
+++ settings.py
@@ -15,6 +15,31 @@ ENVIRONMENTS = {
     }
 }
 
+# The data directory used to store your backtesting CSV files
+CSV_DATA_DIR = "D:\\Backtesting_data\\csv"
+
+# The directory where the backtest.csv and equity.csv files
+# will be stored after a backtest is carried out
+OUTPUT_RESULTS_DIR = "D:\\Backtesting_data\\results"
+
+# Change DOMAIN to "real" if you wish to carry out live trading
+DOMAIN = "practice"
+STREAM_DOMAIN = ENVIRONMENTS["streaming"][DOMAIN]
+API_DOMAIN = ENVIRONMENTS["api"][DOMAIN]
+
+# Your OANDA API Access Token (found in your Account Details on their website)
+ACCESS_TOKEN = "6c05d53507c7b52c6157538bf3573d14-b405ec354aa02999dabd4b736ed677af"
+
+# Your OANDA Account ID (found in your Account Details on their website)
+ACCOUNT_ID = "2105062"
+
+# Your base currency (e.g. "GBP", "USD", "EUR" etc.)
+BASE_CURRENCY = "EUR"
+
+# Your account equity in the base currency (for backtesting)
+EQUITY = Decimal("100000.00")
+
+"""
 CSV_DATA_DIR = os.environ.get('QSFOREX_CSV_DATA_DIR', None)
 OUTPUT_RESULTS_DIR = os.environ.get('QSFOREX_OUTPUT_RESULTS_DIR', None)
 
@@ -26,3 +51,4 @@ ACCOUNT_ID = os.environ.get('OANDA_API_ACCOUNT_ID', None)
 
 BASE_CURRENCY = "GBP"
 EQUITY = Decimal("100000.00")
+"""
